cb30a08e291c068b28535d8c48918f3774a139e9
jaydainn <37037667+jaydainn@users.noreply.github.com>
2020-12-09 10:49:01 +0100

    update bat

--- upload.bat
+++ upload.bat
@@ -1,3 +1,12 @@
+set month=%date:~4,2%
+if "%month:~0,1%" == " " set month=0%month:~1,1%
+echo month=%month%
+set day=%date:~0,2%
+if "%day:~0,1%" == " " set day=0%day:~1,1%
+echo day=%day%
 
-call git commit -a  -m "#%date%-%time%"
+set datetimef=%year%%month%%day%_%hour%%min%%secs%
+
+
+call git commit -a  -m "#%datetimef%"
 call git push
