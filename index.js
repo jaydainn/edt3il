8cd8603fefff772ae20606a2efd787029378ab1d
jaydainn <37037667+jaydainn@users.noreply.github.com>
2021-09-20 14:18:25 +0200

    #202109Mo_141825

--- index.js
+++ index.js
@@ -8,160 +8,166 @@ var data = "";
 const icsToJson = require('ics-to-json');
 const { exec } = require("child_process");
 const { duration } = require('moment');
+let groups = [{url: "https://eleves.groupe3il.fr/edt_eleves/I2%20Groupe%205%20FA.xml"  , id: "i1g5"} , {url: "https://eleves.groupe3il.fr/edt_eleves/I2%20Groupe%206%20FA.xml"  , id: "i2g6"} ]
 let res;
-fetch("https://eleves.groupe3il.fr/edt_eleves/I2%20Groupe%205%20FA.xml", {
-    headers: {
-        "Content-Type": 'text/plain; charset=UTF-8',
-    }
-})
-    .then(response => response.text())
-    .then((data) => {
 
-        xml2js(data, function (err, result) {
-            res = (JSON.stringify(result));
-            fs.writeFileSync('./test.json', res)
-            //console.dir(res);
-        });
-
-
-        JSON.parse(res).DOCUMENT.GROUPE[0].PLAGES.map((plage) => {
-            plage.SEMAINE.map((sem) => {
-                sem.JOUR.map((day) => {
-                    let date = day.Date[0];
-                    let crenau;
-                    let title;
-                    let salle;
-                    let duration = { hours: 1, minutes: 30 }
-                    day.CRENEAU.map((cren) => {
-                        if (Array.isArray(cren.Activite)) {
-                            //console.log(cren.Creneau[0])
-                            title = cren.Activite[0];
-                            crenau = parseInt(cren.Creneau[0]);
-                            salle = cren.Salles[0];
-
-                        } else {
-                            title = undefined;
-                            crenau = undefined;
-                            salle = undefined;
-                        }
-                        if (crenau != undefined) {
-                            //console.log(date , title , crenau);
-                            crenarr.map((el) => {
-                                if (el.id == crenau) {
-                                    let arrdate = date.split('/');
-                                    let arrtime = el.start.split(':');
-                                    //console.log(arrtime);
-                                    events.push({
-                                        title: title,
-                                        start: [parseInt(arrdate[2]), parseInt(arrdate[1]), parseInt(arrdate[0]), parseInt(arrtime[0]), parseInt(arrtime[1])],
-                                        duration: duration,
-                                        location: salle
-                                    })
-                                }
-                            })
-
-                        }
-                    })
-
-                })
-
-            })
-
-
-
-
-            const { error, value } = ics.createEvents(events)
-
-            if (error) {
-                console.log(error)
-                return
-            }
-
-
-
-            console.log(events)
-
-            //console.log(icsToJson.default)
-            const prevEvents = icsToJson.default(fs.readFileSync('./i1g5.ics', 'utf-8'));
-            prevEvents.map((prevev) => {
-                events.map((ev) => {
-                    let hour = ev.start[3] - 1;
-                     hour = hour < 10 ? '0' + hour : hour.toString();
-                    let minutes = ev.start[4] < 10 ? '0' + ev.start[4] : ev.start[4].toString();
-                    let day = ev.start[2] < 10 ? '0' + ev.start[2] : ev.start[2].toString();
-                    let month = ev.start[1] < 10 ? '0' + ev.start[1] : ev.start[1].toString();
-
-                    let evdate = ev.start[0].toString() + month + day + 'T' + hour + minutes + '00Z'
-                    // console.log(evdate + " "+ prevev.startDate)
-                    if (prevev.summary == ev.title && prevev.startDate == evdate) {
-                        console.log(true + " " + evdate + " " + ev.title + " " + prevev.summary + " " + prevev.startDate)
-                        let index = events.indexOf(ev);
-                        events.splice(index, 1);
-                    } else {
-                        console.log(false + " " + evdate + " " + ev.title + " " + prevev.summary + " " + prevev.startDate)
-                    }
-                    // console.log(ev)
-                })
-
-            })
-
-            if (events.length > 0) {
-                if(prevEvents){
-                prevEvents.map((ev) => {
-                    let datestr = ev.startDate.replace('00Z' , "").replace('T' , '');
-                    let year = parseInt(datestr.substring(0,4));
-                    let month = parseInt(datestr.substring(4 , 6));
-                    let day = parseInt(datestr.substring(6 , 8))
-                    let hour = parseInt(datestr.substring(8 , 10)) + 1
-                    let mins = parseInt(datestr.substring(10 , 12));
-                    //console.log(datestr)
-                    //console.log(year+" "+month+" "+day+" "+hour+" "+mins)
-
-                    let timedat = [year , month , day , hour , mins];
-                    let tit = ev.summary;
-                    events.push({
-                        title: tit,
-                        start: timedat,
-                        location: ev.location , 
-                        duration: duration
-                    })
-                   //console.log(ev.location)
-                })
-            }
-
-                fs.writeFileSync("./i1g5.ics", value);
-            }
-
-            console.log(" # of events "+ events.length);
-
-            exec("upload.bat", (error, stdout, stderr) => {
-                if (error) {
-                    console.log(`error: ${error.message}`);
-                    return;
-                }
-                if (stderr) {
-                    console.log(`stderr: ${stderr}`);
-                    return;
-                }
-                console.log(`stdout: ${stdout}`);
+groups.map(group => {
+    fetch( group.url, {
+        headers: {
+            "Content-Type": 'text/plain; charset=UTF-8',
+        }
+    })
+        .then(response => response.text())
+        .then((data) => {
+    
+            xml2js(data, function (err, result) {
+                res = (JSON.stringify(result));
+                fs.writeFileSync('./test.json', res)
+                //console.dir(res);
             });
-
-
-
-
-
+    
+    
+            JSON.parse(res).DOCUMENT.GROUPE[0].PLAGES.map((plage) => {
+                plage.SEMAINE.map((sem) => {
+                    sem.JOUR.map((day) => {
+                        let date = day.Date[0];
+                        let crenau;
+                        let title;
+                        let salle;
+                        let duration = { hours: 1, minutes: 30 }
+                        day.CRENEAU.map((cren) => {
+                            if (Array.isArray(cren.Activite)) {
+                                //console.log(cren.Creneau[0])
+                                title = cren.Activite[0];
+                                crenau = parseInt(cren.Creneau[0]);
+                                salle = cren.Salles[0];
+    
+                            } else {
+                                title = undefined;
+                                crenau = undefined;
+                                salle = undefined;
+                            }
+                            if (crenau != undefined) {
+                                //console.log(date , title , crenau);
+                                crenarr.map((el) => {
+                                    if (el.id == crenau) {
+                                        let arrdate = date.split('/');
+                                        let arrtime = el.start.split(':');
+                                        //console.log(arrtime);
+                                        events.push({
+                                            title: title,
+                                            start: [parseInt(arrdate[2]), parseInt(arrdate[1]), parseInt(arrdate[0]), parseInt(arrtime[0]), parseInt(arrtime[1])],
+                                            duration: duration,
+                                            location: salle
+                                        })
+                                    }
+                                })
+    
+                            }
+                        })
+    
+                    })
+    
+                })
+    
+    
+    
+    
+                const { error, value } = ics.createEvents(events)
+    
+                if (error) {
+                    console.log(error)
+                    return
+                }
+    
+    
+    
+                console.log(events)
+    
+                //console.log(icsToJson.default)
+                const prevEvents = icsToJson.default(fs.readFileSync('./i1g5.ics', 'utf-8'));
+                prevEvents.map((prevev) => {
+                    events.map((ev) => {
+                        let hour = ev.start[3] - 1;
+                         hour = hour < 10 ? '0' + hour : hour.toString();
+                        let minutes = ev.start[4] < 10 ? '0' + ev.start[4] : ev.start[4].toString();
+                        let day = ev.start[2] < 10 ? '0' + ev.start[2] : ev.start[2].toString();
+                        let month = ev.start[1] < 10 ? '0' + ev.start[1] : ev.start[1].toString();
+    
+                        let evdate = ev.start[0].toString() + month + day + 'T' + hour + minutes + '00Z'
+                        // console.log(evdate + " "+ prevev.startDate)
+                        if (prevev.summary == ev.title && prevev.startDate == evdate) {
+                            console.log(true + " " + evdate + " " + ev.title + " " + prevev.summary + " " + prevev.startDate)
+                            let index = events.indexOf(ev);
+                            events.splice(index, 1);
+                        } else {
+                            console.log(false + " " + evdate + " " + ev.title + " " + prevev.summary + " " + prevev.startDate)
+                        }
+                        // console.log(ev)
+                    })
+    
+                })
+    
+                if (events.length > 0) {
+                    if(prevEvents){
+                    prevEvents.map((ev) => {
+                        let datestr = ev.startDate.replace('00Z' , "").replace('T' , '');
+                        let year = parseInt(datestr.substring(0,4));
+                        let month = parseInt(datestr.substring(4 , 6));
+                        let day = parseInt(datestr.substring(6 , 8))
+                        let hour = parseInt(datestr.substring(8 , 10)) + 1
+                        let mins = parseInt(datestr.substring(10 , 12));
+                        //console.log(datestr)
+                        //console.log(year+" "+month+" "+day+" "+hour+" "+mins)
+    
+                        let timedat = [year , month , day , hour , mins];
+                        let tit = ev.summary;
+                        events.push({
+                            title: tit,
+                            start: timedat,
+                            location: ev.location , 
+                            duration: duration
+                        })
+                       //console.log(ev.location)
+                    })
+                }
+    
+                    fs.writeFileSync("./" + group.id+".ics", value);
+                }
+    
+                console.log(" # of events "+ events.length);
+    
+                exec("upload.bat", (error, stdout, stderr) => {
+                    if (error) {
+                        console.log(`error: ${error.message}`);
+                        return;
+                    }
+                    if (stderr) {
+                        console.log(`stderr: ${stderr}`);
+                        return;
+                    }
+                    console.log(`stdout: ${stdout}`);
+                });
+    
+    
+    
+    
+    
+            })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
         })
 
+})
 
-
-
-
-
-
-
-
-
-    })
 
 
 
